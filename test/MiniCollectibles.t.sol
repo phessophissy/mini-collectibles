786d64591bda34fef31df3a4bb354abfc558012b
phessophissy <adefesobi@yahoo.com>
2026-01-23 07:49:02 +0100

    test: add more unit tests

--- test/MiniCollectibles.t.sol
+++ test/MiniCollectibles.t.sol
@@ -13,4 +13,16 @@ contract MiniCollectiblesTest {
     function testMintPrice() public view {
         assert(collectibles.getMintPrice() == 0.000012 ether);
     }
+    
+    function testInitialSupply() public view {
+        assert(collectibles.totalSupply() == 0);
+    }
+    
+    function testName() public view {
+        assert(keccak256(bytes(collectibles.name())) == keccak256(bytes("Mini Collectibles")));
+    }
+    
+    function testSymbol() public view {
+        assert(keccak256(bytes(collectibles.symbol())) == keccak256(bytes("MCOL")));
+    }
 }
