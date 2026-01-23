7d612b9fc5b64139f3638f8f753fbf0b9d0b714d
phessophissy <adefesobi@yahoo.com>
2026-01-23 07:49:15 +0100

    feat: enhance deploy script with baseURI option

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,9 +3,19 @@ pragma solidity ^0.8.24;
 
 import "../src/MiniCollectibles.sol";
 
+/// @title Deploy Script for Mini Collectibles
+/// @notice Deploys the MiniCollectibles contract to Base Chain
 contract DeployScript {
+    MiniCollectibles public collectibles;
+    
     function run() external returns (MiniCollectibles) {
-        MiniCollectibles collectibles = new MiniCollectibles();
+        collectibles = new MiniCollectibles();
+        return collectibles;
+    }
+    
+    function deployWithBaseURI(string memory baseURI) external returns (MiniCollectibles) {
+        collectibles = new MiniCollectibles();
+        collectibles.setBaseURI(baseURI);
         return collectibles;
     }
 }
